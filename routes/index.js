2310092478cedd7c130af38ab94830c1ca061758
WalrusCow <willgmcd@gmail.com>
2014-01-09 17:31:44 -0500

    Random background image; header and footer

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,21 @@
  * Index route. Also expose all other routes.
  */
 
+var path = require('path');
+var fs = require('fs');
+
+function randInt(min, max) {
+  /* Return random integer in interval [min, max]. */
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 exports.index = function(req, res) {
-  res.render('index', { title: 'William McDonald' });
+  // Generate a random background
+  var rootDir = path.dirname(__dirname);
+  var bgFiles = fs.readdirSync(path.join(rootDir, 'public', 'images', 'bg'));
+  console.log(bgFiles);
+  var bgIndex = randInt(0, bgFiles.length - 1);
+  var bg = bgFiles[bgIndex];
+
+  res.render('index', { title: 'William McDonald', background: bg });
 };
