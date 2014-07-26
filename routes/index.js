595d57303bcdfba41a53526922d1c8d7761218b6
WalrusCow <willgmcd@gmail.com>
2014-07-25 21:56:44 -0700

    many change so wow

--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,36 @@ var bgSelect = require('./bgSelect');
 var getRoutes = {
   '/': {
     view: 'index',
-    title: 'William McDonald'
+    title: 'William McDonald',
+    nav: 'home',
   },
   '/projects': {
     view: 'projects',
-    title: 'Projects'
+    title: 'Projects',
+    nav: 'projects',
   },
   '/projects/life': {
     view: 'life',
-    title: 'Game of Life'
+    title: 'Game of Life',
+    nav: 'projects',
   },
   '/projects/tetvas': {
     view: 'tetvas',
-    title: 'Tetvas'
+    title: 'Tetvas',
+    nav: 'projects',
   },
   '/projects/snake': {
     view: 'snake',
-    title: 'Snake'
-  }
+    title: 'Snake',
+    nav: 'projects',
+  },
 };
 
 function createRoute(info) {
   /* Return a new routing function based on `info`. */
   return function(req, res) {
     var bg = bgSelect();
-    res.render(info.view, { title: info.title, background: bg });
+    res.render(info.view, { title: info.title, background: bg, nav: info.nav });
   };
 }
 
